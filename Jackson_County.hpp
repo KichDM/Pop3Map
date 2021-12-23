76c502ae7ae7952575898f31eb8aea4030b2409c
KichDM <79092622+KichDM@users.noreply.github.com>
2021-12-23 19:09:43 +0000

    base

---
+++ Jackson_County.hpp
@@ -0,0 +1,840 @@
+class Applewood
+{
+	name="Lakewood";
+	position[]={8659.78,6975.07};
+	type="NameVillage";
+	
+	radiusA=156.92;
+	radiusB=159.26;
+	angle=0.000;
+};
+class Lakeside
+{
+	name="Lakeside";
+	position[]={8548.57,6567.32};
+	type="NameCityCapital";
+	
+	radiusA=505.00;
+	radiusB=408.03;
+	angle=280.000;
+};
+class Oldtown
+{
+	name="Old Town";
+	position[]={8201.47,6135.73};
+	type="NameVillage";
+	
+	radiusA=270.80;
+	radiusB=252.50;
+	angle=25.000;
+};
+class Business
+{
+	name="Lakeside Business Park";
+	position[]={8532.90,6385.86};
+	type="NameVillage";
+	
+	radiusA=203.11;
+	radiusB=204.93;
+	angle=0.000;
+};
+class Policia
+{
+	name=" Policia Central de Lakeside  ";
+	position[]={8718.81,7205.67};
+	type="StrongpointArea";
+	
+	radiusA=165.59;
+	radiusB=174.74;
+	angle=0.000;
+};
+class Raceway
+{
+	name="Lakeside Circuit";
+	position[]={8941.68,7572.43};
+	type="NameLocal";
+	
+	radiusA=488.89;
+	radiusB=657.56;
+	angle=290.000;
+};
+class Stadium
+{
+	name="Lakeside Stadium";
+	position[]={8460.36,6234.09};
+	type="NameLocal";
+	
+	radiusA=133.97;
+	radiusB=168.16;
+	angle=0.000;
+};
+class PaloHeights
+{
+	name="Palomina Heights";
+	position[]={7956.41,6587.69};
+	type="ViewPoint";
+	
+	radiusA=245.19;
+	radiusB=118.02;
+	angle=0.000;
+};
+class Smallville
+{
+	name="Smallville";
+	position[]={8865.13,8255.66};
+	type="NameCity";
+	
+	radiusA=223.66;
+	radiusB=114.18;
+	angle=0.000;
+};
+class sanmarcos
+{
+	name="San Marcos Air National Guard Base";
+	position[]={9433.19,9106.24};
+	type="Airport";
+	
+	radiusA=829.11;
+	radiusB=707.59;
+	angle=320.000;
+};
+class Farm1
+{
+	name="Depuradora de Lakeside ";
+	position[]={9435.79,9827.41};
+	type="NameCity";
+	
+	radiusA=507.02;
+	radiusB=352.90;
+	angle=0.000;
+};
+class projects
+{
+	name="The Projects";
+	position[]={8244.64,6336.36};
+	type="NameLocal";
+	
+	radiusA=117.81;
+	radiusB=87.24;
+	angle=310.000;
+};
+class centralpark
+{
+	name="Chepi Park";
+	position[]={8621.56,6760.42};
+	type="VegetationBroadleaf";
+	
+	radiusA=72.23;
+	radiusB=117.82;
+	angle=0.000;
+};
+class Rufuspark
+{
+	name="SNV RPG";
+	position[]={8718.88,6604.97};
+	type="VegetationBroadleaf";
+	
+	radiusA=193.95;
+	radiusB=183.71;
+	angle=0.000;
+};
+class marina
+{
+	name="Lakeside Marina";
+	position[]={8142.34,5987.81};
+	type="NameLocal";
+	
+	radiusA=257.08;
+	radiusB=195.78;
+	angle=0.000;
+};
+class ojuela
+{
+	name="Ojuela";
+	position[]={9739.13,918.75};
+	type="NameVillage";
+	
+	radiusA=591.89;
+	radiusB=346.20;
+	angle=0.000;
+};
+class bighorn
+{
+	name="Big Horn Peak";
+	position[]={9257.50,1665.31};
+	type="Hill";
+	
+	radiusA=187.06;
+	radiusB=161.93;
+	angle=0.000;
+};
+class littlehorn
+{
+	name="Little Horn Peak";
+	position[]={8640.47,1675.08};
+	type="Hill";
+	
+	radiusA=248.48;
+	radiusB=214.98;
+	angle=0.000;
+};
+class deadhorse
+{
+	name="Dead Horse Pass";
+	position[]={8957.50,1697.66};
+	type="ViewPoint";
+	
+	radiusA=253.73;
+	radiusB=355.58;
+	angle=0.000;
+};
+class loscerros
+{
+	name="Los Cerros";
+	position[]={8127.97,2453.26};
+	type="NameVillage";
+	
+	radiusA=483.05;
+	radiusB=279.95;
+	angle=0.000;
+};
+class selvanegra
+{
+	name="Selva Negra";
+	position[]={6650.05,1307.93};
+	type="VegetationFir";
+	
+	radiusA=2041.63;
+	radiusB=1389.00;
+	angle=0.000;
+};
+class sancristo
+{
+	name="Nueva San Cristobal";
+	position[]={5028.74,1138.75};
+	type="NameCityCapital";
+	
+	radiusA=340.22;
+	radiusB=335.93;
+	angle=0.000;
+};
+class EZLNHQ
+{
+	name="EZLN HQ";
+	position[]={4935.38,1043.41};
+	type="StrongpointArea";
+	
+	radiusA=63.31;
+	radiusB=52.04;
+	angle=0.000;
+};
+class Morrison
+{
+	name="Nueva Morrison";
+	position[]={5259.89,2976.25};
+	type="NameVillage";
+	
+	radiusA=441.13;
+	radiusB=432.75;
+	angle=0.000;
+};
+class santamaria
+{
+	name="Santa Maria";
+	position[]={3765.47,2118.68};
+	type="NameVillage";
+	
+	radiusA=258.80;
+	radiusB=226.01;
+	angle=0.000;
+};
+class prison
+{
+	name="Bahia cochinos";
+	position[]={9630.29,10067.17};
+	type="StrongpointArea";
+	
+	radiusA=227.77;
+	radiusB=121.20;
+	angle=0.000;
+};
+class forthood
+{
+	name="Fort Hood";
+	position[]={2776.86,2694.10};
+	type="StrongpointArea";
+	
+	radiusA=191.75;
+	radiusB=149.31;
+	angle=0.000;
+};
+class heights2
+{
+	name="Jefferson Hill";
+	position[]={2880.95,5663.36};
+	type="NameVillage";
+	
+	radiusA=481.63;
+	radiusB=394.36;
+	angle=0.000;
+};
+class LosDiablos
+{
+	name=" Los Diablos";
+	position[]={3393.48,7641.27};
+	type="NameCity";
+	
+	radiusA=622.61;
+	radiusB=561.18;
+	angle=0.000;
+};
+class airport1
+{
+	name="Los Diablos International Airport";
+	position[]={2551.67,7643.43};
+	type="Airport";
+	
+	radiusA=1197.19;
+	radiusB=263.56;
+	angle=0.000;
+};
+class losdiablospolice
+{
+	name=" Policia Los  Diablos";
+	position[]={3291.53,7892.84};
+	type="StrongpointArea";
+	
+	radiusA=48.72;
+	radiusB=81.50;
+	angle=0.000;
+};
+class hospital2
+{
+	name="Los Diablos Hospital";
+	position[]={3326.66,7839.20};
+	type="NameLocal";
+	
+	radiusA=76.81;
+	radiusB=71.67;
+	angle=0.000;
+};
+class hospital1
+{
+	name="Lakeside Hospital";
+	position[]={8481.18,6447.86};
+	type="NameLocal";
+	
+	radiusA=81.98;
+	radiusB=93.68;
+	angle=0.000;
+};
+class ranch
+{
+	name="Hoover Ranch";
+	position[]={9111.81,8227.01};
+	type="VegetationVineyard";
+	
+	radiusA=103.59;
+	radiusB=75.31;
+	angle=0.000;
+};
+class pecos
+{
+	name="Pecos ";
+	position[]={519.81,1240.61};
+	type="Airport";
+	
+	radiusA=723.13;
+	radiusB=594.69;
+	angle=0.000;
+};
+class fortlake
+{
+	name="Fort Walden National Historic Site";
+	position[]={6488.34,4131.10};
+	type="ViewPoint";
+	
+	radiusA=304.81;
+	radiusB=290.34;
+	angle=0.000;
+};
+class hacker
+{
+	name="Hacker Gulch";
+	position[]={5642.64,3997.25};
+	type="NameMarine";
+	
+	radiusA=219.56;
+	radiusB=207.94;
+	angle=0.000;
+};
+class panther
+{
+	name="Panther Gulch";
+	position[]={5547.72,3481.93};
+	type="NameMarine";
+	
+	radiusA=277.67;
+	radiusB=244.10;
+	angle=0.000;
+};
+class manzanita
+{
+	name="Manzanita Creek";
+	position[]={5123.53,2376.61};
+	type="NameMarine";
+	
+	radiusA=387.47;
+	radiusB=177.72;
+	angle=0.000;
+};
+class carberry
+{
+	name="Carberry Creek";
+	position[]={4483.97,2142.59};
+	type="NameMarine";
+	
+	radiusA=218.02;
+	radiusB=170.48;
+	angle=0.000;
+};
+class cougar
+{
+	name="Quik Hill";
+	position[]={3866.64,2804.24};
+	type="NameMarine";
+	
+	radiusA=156.02;
+	radiusB=133.29;
+	angle=0.000;
+};
+class squaw
+{
+	name="Squaw Creek";
+	position[]={6983.53,4393.86};
+	type="NameMarine";
+	
+	radiusA=244.17;
+	radiusB=191.71;
+	angle=0.000;
+};
+class spring
+{
+	name="Spring Gulch";
+	position[]={8365.89,3770.29};
+	type="NameMarine";
+	
+	radiusA=635.69;
+	radiusB=236.11;
+	angle=0.000;
+};
+class hart
+{
+	name="Arribas Park";
+	position[]={7089.50,5502.77};
+	type="VegetationBroadleaf";
+	
+	radiusA=532.77;
+	radiusB=403.61;
+	angle=0.000;
+};
+class grouse
+{
+	name="Grouse Creek";
+	position[]={6909.88,5216.21};
+	type="NameMarine";
+	
+	radiusA=452.05;
+	radiusB=351.14;
+	angle=0.000;
+};
+class french
+{
+	name="French Gulch";
+	position[]={8635.45,5140.94};
+	type="NameMarine";
+	
+	radiusA=339.03;
+	radiusB=232.07;
+	angle=0.000;
+};
+class kanaka
+{
+	name="Kanaka Gulch";
+	position[]={8112.63,7292.77};
+	type="RockArea";
+	
+	radiusA=561.02;
+	radiusB=419.75;
+	angle=0.000;
+};
+class black
+{
+	name="Villa del Carbon";
+	position[]={8299.31,9172.57};
+	type="RockArea";
+	
+	radiusA=704.30;
+	radiusB=304.72;
+	angle=0.000;
+};
+class mule
+{
+	name="Mule Creek";
+	position[]={9931.77,9647.63};
+	type="RockArea";
+	
+	radiusA=384.69;
+	radiusB=293.25;
+	angle=0.000;
+};
+class kinney
+{
+	name="Neoxan Creek";
+	position[]={8759.31,9961.71};
+	type="RockArea";
+	
+	radiusA=643.25;
+	radiusB=428.83;
+	angle=0.000;
+};
+class carberryg
+{
+	name="Carberry Gulch";
+	position[]={3488.12,4770.72};
+	type="RockArea";
+	
+	radiusA=882.89;
+	radiusB=817.30;
+	angle=0.000;
+};
+class bh
+{
+	name="Brotherhood Heights";
+	position[]={1844.34,8786.60};
+	type="Hill";
+	
+	radiusA=518.64;
+	radiusB=407.64;
+	angle=0.000;
+};
+class cemetery
+{
+	name="Cementerio La Paz";
+	position[]={4247.94,6507.66};
+	type="ViewPoint";
+	
+	radiusA=73.06;
+	radiusB=65.20;
+	angle=0.000;
+};
+class deathroad
+{
+	name="Carretera de la Muerte";
+	position[]={5529.20,8019.12};
+	type="NameLocal";
+	
+	radiusA=462.89;
+	radiusB=320.30;
+	angle=320.000;
+};
+class ACDCLane
+{
+	name="ACDC Lane";
+	position[]={8607.73,6475.06};
+	type="NameLocal";
+	
+	radiusA=200.00;
+	radiusB=25.00;
+	angle=280.000;
+};
+class kennedy
+{
+	name="Kennedy Ave";
+	position[]={8536.48,6488.56};
+	type="NameLocal";
+	
+	radiusA=200.00;
+	radiusB=25.00;
+	angle=280.000;
+};
+class lincoln
+{
+	name="Lincoln Rd";
+	position[]={8454.11,6500.30};
+	type="NameLocal";
+	
+	radiusA=200.00;
+	radiusB=25.00;
+	angle=280.000;
+};
+class ParkAve
+{
+	name="Park Ave";
+	position[]={8662.92,6816.49};
+	type="NameLocal";
+	
+	radiusA=200.00;
+	radiusB=25.00;
+	angle=10.000;
+};
+class 10st
+{
+	name="10th St";
+	position[]={8533.48,6757.30};
+	type="NameLocal";
+	
+	radiusA=100.00;
+	radiusB=15.00;
+	angle=10.000;
+};
+class 9st
+{
+	name="9th St";
+	position[]={8529.62,6714.64};
+	type="NameLocal";
+	
+	radiusA=100.00;
+	radiusB=15.00;
+	angle=10.000;
+};
+class 8th
+{
+	name="8th St";
+	position[]={8506.08,6597.50};
+	type="NameLocal";
+	
+	radiusA=100.00;
+	radiusB=15.00;
+	angle=10.000;
+};
+class 7th
+{
+	name="7th St";
+	position[]={8567.78,6460.87};
+	type="NameLocal";
+	
+	radiusA=100.00;
+	radiusB=15.00;
+	angle=10.000;
+};
+class 6th
+{
+	name="6th St";
+	position[]={8472.72,6408.79};
+	type="NameLocal";
+	
+	radiusA=100.00;
+	radiusB=15.00;
+	angle=10.000;
+};
+class 5th
+{
+	name="5th St";
+	position[]={8461.22,6318.13};
+	type="NameLocal";
+	
+	radiusA=100.00;
+	radiusB=15.00;
+	angle=10.000;
+};
+class Patton
+{
+	name="Patton Rd";
+	position[]={8268.77,6310.76};
+	type="NameLocal";
+	
+	radiusA=100.00;
+	radiusB=15.00;
+	angle=310.000;
+};
+class 4th
+{
+	name="4th St";
+	position[]={8284.26,6384.06};
+	type="NameLocal";
+	
+	radiusA=100.00;
+	radiusB=15.00;
+	angle=40.000;
+};
+class 3rd
+{
+	name="3rd St";
+	position[]={8214.17,6295.45};
+	type="NameLocal";
+	
+	radiusA=100.00;
+	radiusB=15.00;
+	angle=40.000;
+};
+class 2nd
+{
+	name="2nd St";
+	position[]={8177.14,6246.52};
+	type="NameLocal";
+	
+	radiusA=100.00;
+	radiusB=15.00;
+	angle=40.000;
+};
+class 1st
+{
+	name="1st St";
+	position[]={8115.63,6140.71};
+	type="NameLocal";
+	
+	radiusA=100.00;
+	radiusB=15.00;
+	angle=25.000;
+};
+class NewHaven
+{
+	name="New Haven";
+	position[]={5873.44,8235.90};
+	type="NameCity";
+	
+	radiusA=524.69;
+	radiusB=318.85;
+	angle=0.000;
+};
+class Skate
+{
+	name="SNV RPG";
+	position[]={8577.75,6901.54};
+	type="ViewPoint";
+	
+	radiusA=63.27;
+	radiusB=42.69;
+	angle=0.000;
+};
+class springcliff
+{
+	name="Springfield Cat";
+	position[]={8097.37,5261.36};
+	type="NameVillage";
+	
+	radiusA=585.23;
+	radiusB=389.48;
+	angle=0.000;
+};
+class Triangle
+{
+	name="Triangle Point";
+	position[]={8383.06,6368.64};
+	type="NameLocal";
+	
+	radiusA=129.98;
+	radiusB=97.78;
+	angle=0.000;
+};
+class Bedford
+{
+	name="Old Bedford";
+	position[]={6685.13,3822.12};
+	type="NameVillage";
+	
+	radiusA=665.14;
+	radiusB=227.31;
+	angle=0.000;
+};
+class Devils
+{
+	name="Devil's Sinkhole";
+	position[]={498.31,580.31};
+	type="RockArea";
+	
+	radiusA=181.14;
+	radiusB=112.74;
+	angle=0.000;
+};
+class barranquillas
+{
+	name="Las Barranquillas";
+	position[]={8854.80,6456.58};
+	type="Hill";
+	
+	radiusA=206.31;
+	radiusB=140.94;
+	angle=0.000;
+};
+class minas
+{
+	name="Cuenca minera los bigotes";
+	position[]={9468.01,4138.71};
+	type="Hill";
+	
+	radiusA=233.00;
+	radiusB=159.18;
+	angle=0.000;
+};
+class param
+{
+	name="Zona Paramilitar ";
+	position[]={9986.14,4846.98};
+	type="Hill";
+	
+	radiusA=76.35;
+	radiusB=52.16;
+	angle=0.000;
+};
+class DefaultKeyPoint1
+{
+	name="Zona de Carga Los Cerros";
+	position[]={9554.28,3531.06};
+	type="Hill";
+	
+	radiusA=76.35;
+	radiusB=52.16;
+	angle=0.000;
+};
+class DefaultKeyPoint2
+{
+	name="Zona de Carga San Cristobal";
+	position[]={4688.13,1225.01};
+	type="Hill";
+	
+	radiusA=95.44;
+	radiusB=65.20;
+	angle=0.000;
+};
+class DefaultKeyPoint3
+{
+	name="Zona de Carga Carberry";
+	position[]={3477.28,4531.30};
+	type="Hill";
+	
+	radiusA=291.25;
+	radiusB=198.97;
+	angle=0.000;
+};
+class DefaultKeyPoint4
+{
+	name="Zona de Carga Los Diablos";
+	position[]={3562.96,7781.70};
+	type="Hill";
+	
+	radiusA=364.06;
+	radiusB=248.72;
+	angle=0.000;
+};
+class DefaultKeyPoint5
+{
+	name="Poligono Industrial Subastalia";
+	position[]={4129.45,4915.97};
+	type="Hill";
+	
+	radiusA=364.06;
+	radiusB=248.72;
+	angle=0.000;
+};
+class D13
+{
+	name="Distrito 13";
+	position[]={530.20,5114.22};
+	type="NameCity";
+	
+	radiusA=399.08;
+	radiusB=294.73;
+	angle=0.000;
+};
